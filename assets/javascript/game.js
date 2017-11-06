156c07ea593eb0e0cef352fe194ed1b24a946588
djsulli24 <djsulli24@gmail.com>
2017-11-05 18:59:19 -0600

    Fixed a couple of bugs. Added more comments. Finished and tested the following functions:
    convertArrayToLowerCase()
    checkArrayMatch()
    gameFunction()

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // Hangman
 
-//Object where keys are strings 0 to 49, and values are titles of musicals:
+//Object where keys are strings 0 to 49, and properties are titles of musicals:
 var wordObject = {
     0:	"Les Miserables",
     1:	"Wicked",
@@ -54,16 +54,22 @@ var wordObject = {
     49:	"Ghost"
 }
 
-wordArray = [];
-blanksArray = [];
-remainingGuesses = 10;
-alphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wordArray = [];
+var blanksArray = [];
+var remainingGuesses = 10;
+var alphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// This function clears any array
 
 function resetArray(array) {
     while (array.length > 0) {
         array.pop();
     }
 }
+
+// This function generates a new wordArray by taking an integer, pulling that integer's value from the 
+// wordObject, which has a list of musicals. Then it splits that string into an array of characters, and sets
+// the global variable wordArray equal to the array
 
 function createWordArray(randomNumber) {
 
@@ -81,6 +87,10 @@ function createWordArray(randomNumber) {
 
 }
 
+// This function takes the wordArray Array, and creates a new array where all alphabet letters
+// in the wordArray are converted to underscores _, but special characters are copied over. This
+// new array is set equal to blanksArray
+
 function createBlanksArray() {
 
     // Resets the blanksArray so that it is empty
@@ -91,7 +101,7 @@ function createBlanksArray() {
     // an alphabetical character. For indices of wordArray where there is a special character (comma,
     // exclamation point, etc.), that character is copied over
 
-    for (i = 0; i < wordArray.length; i++) {
+    for (var i = 0; i < wordArray.length; i++) {
         // The boolean isAlphabet is true if the value of the character at index i of wordArray (converted to lowercase)
         // is equal to a character of the alphabetArray.
         var isAlphabet = alphabetArray.indexOf(wordArray[i].toLowerCase()) != -1;
@@ -107,6 +117,31 @@ function createBlanksArray() {
 
 }
 
+// This function takes an array and returns the same array, but with all lowercase characters
+
+function convertArrayToLowerCase(array) {
+    var newArray = [];
+
+    for (var i = 0; i < array.length; i++) {
+        // The boolean isAlphabet is true if the value of the character at index i of wordArray (converted to lowercase)
+        // is equal to a character of the alphabetArray.
+        var isAlphabet = alphabetArray.indexOf(array[i].toLowerCase()) != -1;
+        
+        if (isAlphabet) {
+            newArray[i] = array[i].toLowerCase();
+        }
+
+        else {
+            newArray[i] = array[i];
+        }
+    }
+
+    return newArray;
+
+}
+
+// This function generates a new random number between 0 and 49, creates a new wordArray,
+// creates a new blanksArray, and resets remainingGuesses to 10;
 
 function resetGame() {
 
@@ -117,21 +152,43 @@ function resetGame() {
     remainingGuesses = 10;
 }
 
-function gameFunction (enteredLetter) {
-    var userGuess = enteredLetter;
-    var startValue = 0;
+// This function checks whether two arrays are an exact match
 
-    // This variable will be TRUE if the user's entered letter is in the wordArray
-    var inTheArray = (wordArray.indexOf(userGuess, startValue) != -1);
+function checkArrayMatch(array1, array2) {
+
+    if (array1.length != array2.length) {
+        console.log("These arrays are different in length");
+        return false;
+    }
+
+    for (var i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+// This function takes an entered letter, determines whether that letter is in the wordArray, then updates
+// the blanksArray, replacing underscores where that letter goes
+
+function gameFunction (enteredLetter) {
+    var userGuess = enteredLetter.toLowerCase();
+    var startValue = 0;
+    var wordArrayLowerCase = convertArrayToLowerCase(wordArray);
+
+    // This variable will be TRUE if the user's entered letter is in the lower case wordArrayLowerCase
+    var inTheArray = (wordArrayLowerCase.indexOf(userGuess, startValue) != -1);
     
-    // If the 
+    // This replaces any underscores with the correct letter guess
     
     if (inTheArray) {
 
-        // 
-
-        while (wordArray.indexOf(userGuess, startValue) != -1) {
-            blanksArray[wordArray.indexOf(userGuess, startValue)] = userGuess;
+        while (wordArrayLowerCase.indexOf(userGuess, startValue) != -1) {
+            var index = wordArrayLowerCase.indexOf(userGuess, startValue);            
+            blanksArray[index] = wordArray[index];
             startValue++;
         }
 
@@ -154,7 +211,7 @@ While (remainingGuesses > 0) {
     //When keystroke is hit, call game function
     
 
-    // If all letters are correctly guessed, display success message, then reset game
+    // If all letters are correctly guessed, display success message, then reset game.
 }
 
 resetGame();
