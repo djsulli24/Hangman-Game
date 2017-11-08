4633299b94176bbf283f1c30ece6ae4fad031f57
djsulli24 <djsulli24@gmail.com>
2017-11-08 09:34:05 -0600

    The basic front-end skeleton has been put in, and each dynamically updated component is housed in its own div on the page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,58 @@
 // Hangman
 
 //Object where keys are strings 0 to 49, and properties are titles of musicals:
-var wordObject = {
-    0:	"Les Miserables",
-    1:	"Wicked",
-    2:	"The Phantom of the Opera",
-    3:	"West Side Story",
-    4:	"Blood Brothers",
-    5:	"The Book of Mormon",
-    6:	"Matilda",
-    7:	"Miss Saigon",
-    8:	"Rent",
-    9:	"Sweeney Todd",
-    10:	"Guys & Dolls",
-    11:	"Priscilla Queen of the Desert",
-    12:	"Jesus Christ Superstar",
-    13:	"Once",
-    14:	"Carousel",
-    15:	"Cats",
-    16:	"From Here to Eternity",
-    17:	"Billy Elliot",
-    18:	"Cabaret",
-    19:	"Company",
-    20:	"Hairspray",
-    21:	"Merrily We Roll Along",
-    22:	"My Fair Lady",
-    23:	"Evita",
-    24:	"Jersey Boys",
-    25:	"Legally Blonde",
-    26:	"Sunset Boulevard",
-    27:	"The Lion King",
-    28:	"Chess",
-    29:	"Chicago",
-    30:	"Gypsy",
-    31:	"Hairspray",
-    32:	"The Sound of Music",
-    33:	"Spring Awakening",
-    34:	"Starlight Express",
-    35:	"Sunday in the Park with George",
-    36:	"Into the Woods",
-    37:	"The Producers",
-    38:	"Top Hat",
-    39:	"A Chorus Line",
-    40:	"A Little Night Music",
-    41:	"Avenue Q",
-    42:	"Love Never Dies",
-    43:	"Mary Poppins",
-    44:	"Oklahoma",
-    45:	"Oliver!",
-    46:	"Chitty Chitty Bang Bang",
-    47:	"Crazy For You",
-    48:	"Fiddler on the Roof",
-    49:	"Ghost"
-}
+var wordObject = [
+    "Les Miserables",
+    "Wicked",
+    "The Phantom of the Opera",
+    "West Side Story",
+    "Blood Brothers",
+    "The Book of Mormon",
+    "Matilda",
+    "Miss Saigon",
+    "Rent",
+    "Sweeney Todd",
+    "Guys & Dolls",
+    "Priscilla Queen of the Desert",
+    "Jesus Christ Superstar",
+    "Once",
+    "Carousel",
+    "Cats",
+    "From Here to Eternity",
+    "Billy Elliot",
+    "Cabaret",
+    "Company",
+    "Hairspray",
+    "Merrily We Roll Along",
+    "My Fair Lady",
+    "Evita",
+    "Jersey Boys",
+    "Legally Blonde",
+    "Sunset Boulevard",
+    "The Lion King",
+    "Chess",
+    "Chicago",
+    "Gypsy",
+    "Hairspray",
+    "The Sound of Music",
+    "Spring Awakening",
+    "Starlight Express",
+    "Sunday in the Park with George",
+    "Into the Woods",
+    "The Producers",
+    "Top Hat",
+    "A Chorus Line",
+    "A Little Night Music",
+    "Avenue Q",
+    "Love Never Dies",
+    "Mary Poppins",
+    "Oklahoma",
+    "Oliver!",
+    "Chitty Chitty Bang Bang",
+    "Crazy For You",
+    "Fiddler on the Roof",
+    "Ghost"
+]
 var wordArray = [];
 var blanksArray = [];
 var guessedLetters = [];
@@ -81,11 +81,8 @@ function createWordArray() {
     var randomNumber = Math.floor(Math.random()*50);
     // Resets the wordArray so that it is empty.
     resetArray(wordArray);
-    // Converts the random number to a string, so that it can be used to retrieve a new
-    // musical from the wordObject Object
-    var numberString = randomNumber.toString();
     // Sets the new "word" variable equal to the string retrieved from the wordObject
-    var word = wordObject[numberString];
+    var word = wordObject[randomNumber];
     // Sets the global variable wordArray equal to an array. where the the string retrieved from the
     // wordObject is split into individual characters
     wordArray = word.split("");
@@ -177,6 +174,7 @@ function checkArrayMatch(array1, array2) {
 
 }
 
+// -------------------------------MAIN GAME FUNCTION------------------------------------------------------
 // This function takes an entered letter, determines whether that letter is in the wordArray, then updates
 // the blanksArray, replacing underscores where that letter goes
 
@@ -221,13 +219,13 @@ function gameFunction (enteredLetter) {
 
     displayValues();
 
-    setTimeout(function() { checkGameOver(); }, 2000);
+    setTimeout(function() { checkGameOver(); }, 100);
 }
 
 // This function displays all the current game values on the page
 
 function displayValues() {
-    document.getElementById("output").innerHTML = blanksArray.join(" ");
+    document.getElementById("output").innerHTML = blanksArray.join("");
     document.getElementById("remaining").innerHTML = "Remaining Guesses: " + remainingGuesses; 
     document.getElementById("guesses").innerHTML = "Guessed Letters: " + guessedLetters.join(", ");     
     document.getElementById("wins").innerHTML = "Wins: " + wins;
